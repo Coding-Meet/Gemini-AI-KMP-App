e571e751e6999feed0a1e7653e4577a8fdb63201
coding meet <codingmeet26@gmail.com>
2024-02-09 13:23:29 +0530

    color change it

--- composeApp/src/commonMain/kotlin/presentation/components/TopBarLayout.kt
+++ composeApp/src/commonMain/kotlin/presentation/components/TopBarLayout.kt
@@ -1,17 +1,12 @@
 package presentation.components
 
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Send
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
