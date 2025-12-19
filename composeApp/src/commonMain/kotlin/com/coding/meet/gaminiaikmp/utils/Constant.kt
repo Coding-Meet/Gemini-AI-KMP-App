0c51491aa794a61f40aca37a52f2afcd40c0e3e3
Meet B <131579912+Coding-Meet@users.noreply.github.com>
2025-12-19 21:08:14 +0530

    Update GEMINI_PRO and GEMINI_PRO_VISION constants

--- composeApp/src/commonMain/kotlin/com/coding/meet/gaminiaikmp/utils/Constant.kt
+++ composeApp/src/commonMain/kotlin/com/coding/meet/gaminiaikmp/utils/Constant.kt
@@ -2,8 +2,8 @@ package com.coding.meet.gaminiaikmp.utils
 
 const val BASE_URL = "https://generativelanguage.googleapis.com/"
 const val DB_NAME = "GeminiApiChatDB.db"
-const val GEMINI_PRO = "gemini-1.5-flash"
-const val GEMINI_PRO_VISION = "gemini-1.5-flash"
+const val GEMINI_PRO = "gemini-2.5-flash"
+const val GEMINI_PRO_VISION = "gemini-2.5-flash"
 enum class TYPE{
     MOBILE,
     DESKTOP,
